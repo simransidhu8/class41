b69111596124a41251e31053fcdc1163fa3f787e
simransidhu8 <70163239+simransidhu8@users.noreply.github.com>
2020-11-17 20:55:04 -0500

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,51 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = 0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance,
+      rank: this.rank
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+
+  getCarEnd(){
+    var carEndInfo = database.ref('carEnd');
+    carEndInfo.on("value", (data)=>{
+      this.rank= data.val();
+    })
+  }
+
+  static updateCarEnd(rank){
+    var rankInfo = database.ref('/');
+    rankInfo.update({
+      carEnd: rank 
+    })
+  }
+}
